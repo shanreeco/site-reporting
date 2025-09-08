7270058fe3302ac009feb2f9b253e72ff23ce3f2
shanreeco <shanreecosocorro@gmail.com>
2025-09-08 11:42:06 +0800

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+diff --git a//dev/null b/postcss.config.js
+index 0000000000000000000000000000000000000000..2e7af2b7f1a6f391da1631d93968a9d487ba977d 100644
+--- a//dev/null
++++ b/postcss.config.js
+@@ -0,0 +1,6 @@
++export default {
++  plugins: {
++    tailwindcss: {},
++    autoprefixer: {},
++  },
++}
