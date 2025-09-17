41c8d4375442a61d861e100326b1e5f374d5061f
shanreeco <shanreecosocorro@gmail.com>
2025-09-17 18:04:58 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/index";
+import ProjectsPage from "./pages/projects";
 import ReportsPage from "./pages/reports";
 
 export default function App() {
-  return <ReportsPage />;
-  }
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/reports" element={<ReportsPage />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
